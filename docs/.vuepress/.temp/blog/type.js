bc60d6a60a80c374e0d9f33c3a7a921b1862ea0e
Dmitry G Vyalkov <dima-mav@yandex.ru>
2024-10-17 21:53:26 +0300

    Remove articles folder

--- docs/.vuepress/.temp/blog/type.js
+++ docs/.vuepress/.temp/blog/type.js
@@ -1,13 +1,2 @@
-export const typesMap = {"article":{"/":{"path":"/article/","indexes":[0,1,2,3,4,5,6,7,8,9,10,11,12,13]}},"timeline":{"/":{"path":"/timeline/","indexes":[2,3,4,5,6,7,8,9,10,11,12,13,1,0,14,15]}}};
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept();
-  if (__VUE_HMR_RUNTIME__.updateBlogType)
-    __VUE_HMR_RUNTIME__.updateBlogType(typesMap);
-}
-
-if (import.meta.hot)
-  import.meta.hot.accept(({ typesMap }) => {
-    __VUE_HMR_RUNTIME__.updateBlogType(typesMap);
-  });
+export const typesMap = {"article":{},"timeline":{}};
 
