907cd07a03bfd1f522726a31959f22286c89579a
Dmitry G Vyalkov <dima-mav@yandex.ru>
2024-10-21 13:10:24 +0300

    add .gitignore

--- docs/.vuepress/.temp/internal/routes.js
+++ docs/.vuepress/.temp/internal/routes.js
@@ -1,6 +1,23 @@
 export const redirects = JSON.parse("{}")
 
 export const routes = Object.fromEntries([
-  ["/", { loader: () => import(/* webpackChunkName: "index.html" */"C:/work/web/klimat/docs/.vuepress/.temp/pages/index.html.js"), meta: {"title":""} }],
-  ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"C:/work/web/klimat/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
+  ["/", { loader: () => import(/* webpackChunkName: "index.html" */"C:/work/web/klimat/docs/.vuepress/.temp/pages/index.html.js"), meta: {} }],
+  ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"C:/work/web/klimat/docs/.vuepress/.temp/pages/404.html.js"), meta: {} }],
 ]);
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updateRoutes) {
+    __VUE_HMR_RUNTIME__.updateRoutes(routes)
+  }
+  if (__VUE_HMR_RUNTIME__.updateRedirects) {
+    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ routes, redirects }) => {
+    __VUE_HMR_RUNTIME__.updateRoutes(routes)
+    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
+  })
+}
--- docs/.vuepress/.temp/internal/siteData.js
+++ docs/.vuepress/.temp/internal/siteData.js
@@ -1,1 +1,14 @@
 export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"uk-UA\",\"title\":\"ТОВ «Клімат Стандарт Плюс»\",\"description\":\"Проєетування. монтаж та обслуговування вентиляційних систем, систем кондиціювання та опалення.\",\"head\":[],\"locales\":{}}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updateSiteData) {
+    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ siteData }) => {
+    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
+  })
+}
